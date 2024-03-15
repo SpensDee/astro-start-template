1dba9325a0c2f92a7cb67d0ae1a9e8872240f4dd
onepjob@gmail.com <onepjob@gmail.com>
2024-03-15 23:07:59 +0200

    sitemap generate

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,9 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
+import astroI18next from "astro-i18next";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://onepartners.io/",
+  integrations: [sitemap(), astroI18next()],
+});
