8737338638dfc0dca1b31641c4737037399c2237
onepjob@gmail.com <onepjob@gmail.com>
2024-03-17 15:57:37 +0200

    lang switcher fix bug for double //

--- src/utils/i18n/i18n.ts
+++ src/utils/i18n/i18n.ts
@@ -1,4 +1,4 @@
-import { ui, defaultLang, showDefaultLang } from "./ui";
+import { ui, defaultLang, showDefaultLang, languages } from "./ui";
 
 // визначення мови сторінки на основі поточної URL-адреси
 export function getLangFromUrl(url: URL) {
@@ -20,3 +20,11 @@ export function useTranslatedPath(lang: keyof typeof ui) {
     return !showDefaultLang && l === defaultLang ? path : `/${l}${path}`;
   };
 }
+
+export function pathAfterLanguage(pathname: URL) {
+  const langKeys = Object.keys(languages);
+  const segments = pathname.pathname.split("/");
+  const langIndex = segments.findIndex(segment => langKeys.includes(segment));
+  return langIndex !== -1 ? segments.slice(langIndex + 1).join('/') : pathname.pathname;
+}
+
